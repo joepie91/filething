b2083174c6126c20b26cda9fb29437bb6afbe230
Sven Slootweg <jamsoftgamedev@gmail.com>
2014-05-21 10:22:28 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='filething',
+      version='1.0.0',
+      description='A thin light-weight wrapper library, to make filesystem operations in Python suck less.',
+      author='Sven Slootweg',
+      author_email='filething@cryto.net',
+      url='https://github.com/joepie91/filething',
+      packages=['filething'],
+      provides=['filething'],
+      license="WTFPL"
+     )
